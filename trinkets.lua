5c4d0fa8b69016db888824343a5302f1601fe8a4
Robin <midouesterner@gmail.com>
2024-05-01 16:20:02 -0500

    trig/clock modes

--- trinkets.lua
+++ trinkets.lua
@@ -1,68 +1,159 @@
 -- flexible midi-to-crow routing
+tabutil = require "tabutil"
+
 function n2v(n) return n / 12 end
 
 function cc2v(cc) return cc / 127 end
 
-common_mode = {}
+function set_trinket_note(trinket, note) trinket.note = note end
 
-function common_mode.note_on(trinket, msg) trinket.note = msg.note end
-
-function common_mode.pitchbend(trinket, msg)
-    trinket.pitchbend = (trinket.pitchbend_range / 12) * (msg.val - 8192) / 8192
+function set_trinket_pitchbend(trinket, pitchbend)
+    trinket.pitchbend = (trinket.pitchbend_range / 12) * (pitchbend - 8192) /
+                            8192
 end
 
-note_mode = {}
-
-function note_mode.note_on(trinket, msg)
+function update_trinket_voct(trinket)
     crow.output[trinket.output].volts =
         trinket.volt_offset + n2v(trinket.note) + trinket.pitchbend
 end
 
-function note_mode.pitchbend(trinket, msg)
-    if trinket.note == nil then return end
-    crow.output[trinket.output].volts =
-        trinket.volt_offset + n2v(trinket.note) + trinket.pitchbend
+function all_notes_off(trinket)
+    trinket.note = nil
+    trinket.pitchbend = 0
+    crow.output[trinket.output].volts = 0
 end
 
-gate_mode = {}
+CHANNEL_SUFFIX = "channel"
+SLEW_RATE_SUFFIX = "slew_rate"
+MODE_SUFFIX = "mode"
+PITCHBEND_RANGE_SUFFIX = "pitchbend_range"
+VOLT_OFFSET_SUFFIX = "volt_offset"
+VOLT_RANGE_SUFFIX = "volt_range"
+CONTROL_SUFFIX = "control"
+PULSE_DURATION_SUFFIX = "pulse_duration"
+CLOCK_DIVISION_SUFFIX = "clock_division"
 
-function gate_mode.note_on(trinket, msg)
-    crow.output[trinket.output].volts = trinket.volt_offset + trinket.volt_range
-end
+COMMON_TRINKET_PARAMS = {CHANNEL_SUFFIX, SLEW_RATE_SUFFIX, MODE_SUFFIX}
 
-function gate_mode.note_off(trinket, msg)
-    if msg.note ~= trinket.note then return end
-    crow.output[trinket.output].volts = trinket.volt_offset
-end
-
-velocity_mode = {}
-
-function velocity_mode.note_on(trinket, msg)
-    crow.output[trinket.output].volts =
-        trinket.volt_offset + trinket.volt_range * cc2v(msg.vel)
-end
-
-function velocity_mode.note_off(trinket, msg)
-    if msg.note ~= trinket.note then return end
-    crow.output[trinket.output].volts = trinket.volt_offset
-end
-
-noop_mode = {}
-
-modes = {
-    {name = "NOTE", mode = note_mode}, {name = "GATE", mode = gate_mode},
-    {name = "VELOCITY", mode = velocity_mode},
-    {name = "CONTROL", mode = noop_mode}
+TRINKET_PARAMS = {
+    PITCHBEND_RANGE_SUFFIX, VOLT_OFFSET_SUFFIX, VOLT_RANGE_SUFFIX,
+    CONTROL_SUFFIX, PULSE_DURATION_SUFFIX, CLOCK_DIVISION_SUFFIX
 }
+
+NoteMode = {params = {PITCHBEND_RANGE_SUFFIX, VOLT_OFFSET_SUFFIX}, event = {}}
+
+function NoteMode.init(trinket) end
+
+function NoteMode.update(trinket) end
+
+function NoteMode.cleanup(trinket) crow.output[trinket.output].volts = 0 end
+
+function NoteMode.event.note_on(trinket, msg)
+    set_trinket_note(trinket, msg.note)
+    update_trinket_voct(trinket)
+end
+
+function NoteMode.event.pitchbend(trinket, msg)
+    if trinket.note == nil then return end
+    set_trinket_pitchbend(trinket, msg.val)
+    update_trinket_voct(trinket)
+end
+
+GateMode = {params = {VOLT_RANGE_SUFFIX}, event = {}}
+
+function GateMode.init(trinket) end
+
+function GateMode.update(trinket) end
+
+function GateMode.cleanup(trinket) crow.output[trinket.output].volts = 0 end
+
+function GateMode.event.note_on(trinket, msg)
+    set_trinket_note(trinket, msg.note)
+    crow.output[trinket.output].volts = trinket.volt_range
+end
+
+function GateMode.event.note_off(trinket, msg)
+    if msg.note ~= trinket.note then return end
+    crow.output[trinket.output].volts = 0
+end
+
+TrigMode = {params = {VOLT_RANGE_SUFFIX, PULSE_DURATION_SUFFIX}, event = {}}
+
+function TrigMode.init(trinket) end
+
+function TrigMode.update(trinket) end
+
+function TrigMode.cleanup(trinket) crow.output[trinket.output].volts = 0 end
+
+function TrigMode.event.note_on(trinket, msg)
+    local action = "{to(" .. tostring(trinket.volt_range) .. ",0),to(0," ..
+                       tostring(trinket.pulse_duration) .. ")}"
+    crow.output[trinket.output].action = action
+    crow.output[trinket.output]()
+end
+
+VelocityMode = {params = {VOLT_RANGE_SUFFIX}, event = {}}
+
+function VelocityMode.init(trinket) end
+
+function VelocityMode.update(trinket) end
+
+function VelocityMode.cleanup(trinket) crow.output[trinket.output].volts = 0 end
+
+function VelocityMode.event.note_on(trinket, msg)
+    set_trinket_note(trinket, msg.note)
+    crow.output[trinket.output].volts = trinket.volt_range * cc2v(msg.vel)
+end
+
+function VelocityMode.event.note_off(trinket, msg)
+    if msg.note ~= trinket.note then return end
+    crow.output[trinket.output].volts = 0
+end
+
+ControlMode = {
+    params = {VOLT_OFFSET_SUFFIX, VOLT_RANGE_SUFFIX, CONTROL_SUFFIX},
+    event = {}
+}
+
+function ControlMode.init(trinket) end
+
+function ControlMode.update(trinket) end
+
+function ControlMode.cleanup(trinket) end
+
+ClockMode = {params = {VOLT_RANGE_SUFFIX, CLOCK_DIVISION_SUFFIX}, event = {}}
+
+function ClockMode.init(trinket)
+    trinket.clock = clock.run(function()
+        while true do
+            clock.sync(1 / trinket.clock_division)
+            crow.output[trinket.output].volts = trinket.volt_range
+            clock.sleep(60 / (2 * clock.get_tempo() * trinket.clock_division))
+            crow.output[trinket.output].volts = 0
+        end
+    end)
+end
+
+function ClockMode.update(trinket) end
+
+function ClockMode.cleanup(trinket)
+    if trinket.clock == nil then return end
+    clock.cancel(trinket.clock)
+    trinket.clock = nil
+end
 
 function update_trinket(trinket, msg)
     if msg.ch ~= trinket.channel then return end
 
-    local common_callback = common_mode[msg.type]
-    if common_callback ~= nil then common_callback(trinket, msg) end
+    if msg.type == "cc" and msg.cc == 120 then
+        all_notes_off(trinket)
+        return
+    end
 
-    local mode_callback = trinket.mode[msg.type]
-    if mode_callback ~= nil then mode_callback(trinket, msg) end
+    local mode_callback = trinket.mode.event[msg.type]
+    if mode_callback == nil then return end
+
+    mode_callback(trinket, msg)
 end
 
 function midi_event(data)
@@ -71,29 +162,63 @@ function midi_event(data)
     for _, trinket in ipairs(trinkets) do update_trinket(trinket, msg) end
 end
 
-mode_options = {}
-for _, mode in ipairs(modes) do table.insert(mode_options, mode.name) end
+MODES = {
+    {name = "NOTE", mode = NoteMode}, {name = "GATE", mode = GateMode},
+    {name = "TRIG", mode = TrigMode}, {name = "VELOCITY", mode = VelocityMode},
+    {name = "CONTROL", mode = ControlMode}, {name = "CLOCK", mode = ClockMode}
+}
 
-function add_trinket(output, mode)
-    local trinket = {output = output}
+NOTE_INDEX = 1
+GATE_INDEX = 2
+TRIG_INDEX = 3
+VELOCITY_INDEX = 4
+CONTROL_INDEX = 5
+CLOCK_INDEX = 6
 
-    params:add_group("TRINKET " .. output, 7)
+MODE_OPTIONS = {}
+for _, mode in ipairs(MODES) do table.insert(MODE_OPTIONS, mode.name) end
+
+function add_trinket(output, mode_index)
+    local trinket = {
+        output = output,
+        note = nil,
+        pitchbend = 0,
+        channel = 1,
+        slew_rate = 0,
+        mode_index = mode_index,
+        mode_name = MODES[mode_index].name,
+        mode = MODES[mode_index].mode,
+        pitchbend_range = 2,
+        volt_offset = 0,
+        volt_range = 10,
+        pulse_duration = 0.005,
+        clock_division = 1
+    }
+
+    params:add_group("trinket_out" .. output .. "_group", "OUTPUT " .. output,
+                     #COMMON_TRINKET_PARAMS + #TRINKET_PARAMS)
 
     params:add{
         type = "number",
-        id = "trinket" .. output .. "_channel",
+        id = "trinket_out" .. output .. "_channel",
         name = "midi channel",
         min = 1,
         max = 16,
-        default = 1,
+        default = trinket.channel,
         action = function(value) trinket.channel = value end
     }
 
     params:add{
         type = "control",
-        id = "trinket" .. output .. "_slew",
+        id = "trinket_out" .. output .. "_slew",
         name = "slew rate",
-        controlspec = controlspec.UNIPOLAR,
+        controlspec = controlspec.def {
+            min = 0,
+            max = 1,
+            warp = "lin",
+            step = 0,
+            default = trinket.slew_rate
+        },
         action = function(value)
             trinket.slew_rate = value
             crow.output[output].slew = value
@@ -102,75 +227,112 @@ function add_trinket(output, mode)
 
     params:add{
         type = "option",
-        id = "trinket" .. output .. "_mode",
+        id = "trinket_out" .. output .. "_mode",
         name = "mode",
-        options = mode_options,
-        default = mode or 1,
+        options = MODE_OPTIONS,
+        default = trinket.mode_index,
         action = function(value)
+            if trinket.mode ~= nil then
+                trinket.mode.cleanup(trinket)
+                for _, suffix in ipairs(trinket.mode.params) do
+                    params:hide("trinket_out" .. output .. "_" .. suffix)
+                end
+            end
+
             trinket.mode_index = value
-            trinket.mode = modes[value].mode
+            trinket.mode_name = MODES[value].name
+            trinket.mode = MODES[value].mode
+            trinket.mode.init(trinket)
+            for _, suffix in ipairs(trinket.mode.params) do
+                params:show("trinket_out" .. output .. "_" .. suffix)
+            end
+            _menu.rebuild_params()
         end
     }
 
     params:add{
         type = "number",
-        id = "trinket" .. output .. "_pitchbend_range",
+        id = "trinket_out" .. output .. "_pitchbend_range",
         name = "pitchbend range",
         min = 0,
         max = 48,
-        default = 2,
-        action = function(value)
-            trinket.pitchbend_range = value
-            trinket.pitchbend = trinket.pitchbend or 0
-        end
-    }
-
-    params:add{
-        type = "control",
-        id = "trinket" .. output .. "_volt_offset",
-        name = "volt offset",
-        controlspec = controlspec.def {
-            min = -5,
-            max = 10,
-            warp = "lin",
-            step = 1,
-            default = 0,
-            units = "v",
-            quantum = 0.01,
-            wrap = false
-        },
-        action = function(value) trinket.volt_offset = value end
-    }
-
-    params:add{
-        type = "control",
-        id = "trinket" .. output .. "_volt_range",
-        name = "volt range",
-        controlspec = controlspec.def {
-            min = -15,
-            max = 15,
-            warp = "lin",
-            step = 1,
-            default = 10,
-            units = "v",
-            quantum = 0.01,
-            wrap = false
-        },
-        action = function(value) trinket.volt_range = value end
+        default = trinket.pitchbend_range,
+        action = function(value) trinket.pitchbend_range = value end
     }
 
     params:add{
         type = "number",
-        id = "trinket" .. output .. "_control",
+        id = "trinket_out" .. output .. "_volt_offset",
+        name = "volt offset",
+        min = -5,
+        max = 0,
+        default = trinket.volt_offset,
+        action = function(value)
+            trinket.volt_offset = value
+            if trinket.mode ~= nil then trinket.mode.update(trinket) end
+        end
+    }
+
+    params:add{
+        type = "number",
+        id = "trinket_out" .. output .. "_volt_range",
+        name = "volt range",
+        min = 0,
+        max = 10,
+        default = trinket.volt_range,
+        action = function(value)
+            trinket.volt_range = value
+            if trinket.mode ~= nil then trinket.mode.update(trinket) end
+        end
+    }
+
+    params:add{
+        type = "number",
+        id = "trinket_out" .. output .. "_control",
         name = "control",
         min = 0,
         max = 127,
         default = 0,
         action = function(value)
+            if trinket.mode_index ~= CONTROL_INDEX then return end
             crow.output[trinket.output].volts =
                 trinket.volt_offset + trinket.volt_range * cc2v(value)
         end
     }
+
+    params:add{
+        type = "control",
+        id = "trinket_out" .. output .. "_pulse_duration",
+        name = "pulse duration",
+        controlspec = controlspec.def {
+            min = 0.001,
+            max = 0.1,
+            warp = "lin",
+            step = 0.001,
+            default = trinket.pulse_duration,
+            units = "",
+            wrap = false
+        },
+        formatter = function(param) return param:get() end,
+        action = function(value)
+            trinket.pulse_duration = value
+            if trinket.mode ~= nil then trinket.mode.update(trinket) end
+        end
+    }
+
+    params:add{
+        type = "number",
+        id = "trinket_out" .. output .. "_clock_division",
+        name = "clock division",
+        min = 1,
+        max = 32,
+        default = trinket.clock_division,
+        action = function(value) trinket.clock_division = value end
+    }
+
+    for _, suffix in ipairs(TRINKET_PARAMS) do
+        params:hide("trinket_out" .. output .. "_" .. suffix)
+    end
 
     return trinket
 end
@@ -179,7 +341,6 @@ function get_midi_devices()
     local devices = {}
     for i = 1, #midi.vports do
         local long_name = midi.vports[i].name
-        -- local short_name = string.len(long_name) > 15 and util.acronym(long_name) or long_name
         table.insert(devices, long_name)
     end
     return devices
@@ -189,11 +350,12 @@ function init()
     midi_devices = get_midi_devices()
     trinkets = {}
 
-    params:add_separator("TRINKETS")
+    params:hide("CLOCK")
+    params:add_separator("trinket_separator", "TRINKETS")
 
     params:add{
         type = "option",
-        id = "midi_in_device",
+        id = "trinket_midi_in_device",
         name = "midi in",
         options = midi_devices,
         default = 1,
@@ -201,11 +363,16 @@ function init()
             midi.cleanup()
             midi_device = midi.connect(value)
             midi_device.event = midi_event
+            params:set("clock_source", 2)
+            params:set("clock_midi_in", value + 2)
         end
     }
+    params:set_save("trinket_midi_in_device", false)
 
-    local trinket_modes = {1, 2, 3, 4}
-    for i, mode in ipairs(trinket_modes) do
+    local default_modes = {
+        NOTE_INDEX, GATE_INDEX, VELOCITY_INDEX, CONTROL_INDEX
+    }
+    for i, mode in ipairs(default_modes) do
         local trinket = add_trinket(i, mode)
         table.insert(trinkets, trinket)
     end
